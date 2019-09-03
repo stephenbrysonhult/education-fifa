d0088cd2b61bbbde3312fa88e291e52f0bc3aaa2
Stephen Bryson <stephen.bryson@ef.com@Stephens-MacBook-Pro.local>
2019-09-03 15:07:03 +0100

    Updates

---
+++ precache-manifest.0d55f45be15d2445b9948100f357e14d.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5db2a400dddfc6f3f886422c6adef158",
+    "url": "/education-fifa/index.html"
+  },
+  {
+    "revision": "77e54f63acc5e1ff7d39",
+    "url": "/education-fifa/static/css/main.acfc854d.chunk.css"
+  },
+  {
+    "revision": "3ad75b9c7655f7e32a67",
+    "url": "/education-fifa/static/js/2.3832993b.chunk.js"
+  },
+  {
+    "revision": "77e54f63acc5e1ff7d39",
+    "url": "/education-fifa/static/js/main.5f49ffad.chunk.js"
+  },
+  {
+    "revision": "6a3ab7b01df26af676c6",
+    "url": "/education-fifa/static/js/runtime~main.cfd99a6d.js"
+  },
+  {
+    "revision": "6e044c5d4a1ee72c5ac6d210ed8c3478",
+    "url": "/education-fifa/static/media/EFCircularWeb-Black.6e044c5d.woff"
+  },
+  {
+    "revision": "467df8e30c06d7e736046fb15e8e004f",
+    "url": "/education-fifa/static/media/EFCircularWeb-Bold.467df8e3.woff"
+  },
+  {
+    "revision": "ed66b0e567dffee06a029be9a900a1f9",
+    "url": "/education-fifa/static/media/EFCircularWeb-Book.ed66b0e5.woff"
+  },
+  {
+    "revision": "c8399aaf02eeebcfcf78525211c7e16d",
+    "url": "/education-fifa/static/media/EFCircularWeb-Medium.c8399aaf.woff"
+  },
+  {
+    "revision": "894b20e83e27954ded36dd933cd06804",
+    "url": "/education-fifa/static/media/amfc.894b20e8.png"
+  },
+  {
+    "revision": "31962d0c0c3b0b063d0788de07ea2030",
+    "url": "/education-fifa/static/media/bmfc.31962d0c.png"
+  },
+  {
+    "revision": "d854acffbd8f25498a02f989608016f9",
+    "url": "/education-fifa/static/media/cfc.d854acff.png"
+  },
+  {
+    "revision": "9a17b93ddcebc75684fffbc575b50205",
+    "url": "/education-fifa/static/media/fcb.9a17b93d.png"
+  },
+  {
+    "revision": "015dd8c278e3ef06575c79d5ddffecb4",
+    "url": "/education-fifa/static/media/imfc.015dd8c2.png"
+  },
+  {
+    "revision": "b5052fbdb1518bda3df3e3c8c1eb6caa",
+    "url": "/education-fifa/static/media/jfc.b5052fbd.png"
+  },
+  {
+    "revision": "d73b731caf9b036535bad7eea156450c",
+    "url": "/education-fifa/static/media/lfc.d73b731c.png"
+  },
+  {
+    "revision": "234c942ff91a5d5d43cc1745297c3fd4",
+    "url": "/education-fifa/static/media/mcfc.234c942f.png"
+  },
+  {
+    "revision": "01fb89c13d6cf99b255b750830cb8175",
+    "url": "/education-fifa/static/media/mufc.01fb89c1.png"
+  },
+  {
+    "revision": "0cb80df2326aaf81d09914fb98da2480",
+    "url": "/education-fifa/static/media/psg.0cb80df2.png"
+  },
+  {
+    "revision": "e36a999d953d093c5826a1515a145911",
+    "url": "/education-fifa/static/media/rmfc.e36a999d.png"
+  },
+  {
+    "revision": "514abec101ccfbdf458b0519869bdb15",
+    "url": "/education-fifa/static/media/thfc.514abec1.png"
+  }
+]);
